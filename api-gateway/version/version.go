a505119adf4deb320454130c0355554a7d2e161a
BackAged <brotherapes@gmail.com>
2020-05-01 14:33:37 +0600

    Bootstrap

---
+++ api-gateway/version/version.go
@@ -0,0 +1,6 @@
+package version
+
+//  Version defines version
+const (
+	Version = "unVersioned"
+)
---
+++ task/domain/task/subtask.go
@@ -0,0 +1,7 @@
+package task
+
+// SubTask defines subTask of Task
+type SubTask struct {
+	Topic       string
+	Description string
+}
